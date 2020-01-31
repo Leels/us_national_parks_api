c8713ebfd15e08f4ce103ea5bdd41d5b425882c5
Lela Smith <smith.lelaa@gmail.com>
2020-01-31 08:57:11 -0800

    add routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :states do
+    resources :parks
+  end
 end
