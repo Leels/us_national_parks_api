d048d3dcea11060eaaab63d710292bbc8e47c2ea
Lela Smith <smith.lelaa@gmail.com>
2020-01-31 09:47:39 -0800

    add code to state controller

---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,34 @@
+class StatesController < ApplicationController
+
+  def index
+    @states = State.all
+    json_response(@states)
+  end
+
+  def show
+    @state = State.find(params[:id])
+    json_response(@state)
+  end
+
+  def create
+    @state = State.create!(state_params)
+    json_response(@state)
+  end
+
+  def update
+    @state = State.find(params[:id])
+    if @state.update!(state_params)
+      render status: 200, json: {message: "State has been successfully updated"}
+  end
+end
+
+  def destroy
+    @state = State.find(params[:id])
+    if @state.destroy!
+      render status: 200, json: { message: "State has been successfully deleted"}
+  end
+end
+
+def state_params
+  params.permit(:name)
+end
